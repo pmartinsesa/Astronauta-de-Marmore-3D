52acbfa5c5efb45d5de8e03b24af7963db05d914
pmartinsesa <pmartinsesa@gmail.com>
2024-10-26 22:22:18 -0300

    chore: refactor playerManager code

---
+++ Assets/Scripts/Player/Editor/PlayerEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Assets.Scripts.Player.Editor
+{
+    [CustomEditor(typeof(PlayerManager))]
+    public class PlayerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI() 
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,60 +4,70 @@ namespace Assets.Scripts.Player
 {
     public class PlayerManager : MonoBehaviour
     {
-        [Header("States")]
         public CharacterController controller;
         public Animator animator;
 
-        public float speed;
-        public float turnSpeed;
-        public float jumpForce;
-        public float jumpLimit;
+        public float speed = 20f;
+        public float turnSpeed = 250f;
+        public float jumpForce = 25f;
+        public float jumpLimit = 5f;
 
-        public float gravity;
+        public float gravity = 1f;
 
-        private float jumpCoeficient;
-        private bool isJumping;
-        private float groundPosition;
+        private float _jumpCoeficient;
+        private bool _isJumping;
+        private float _groundPosition;
+
         private void Start()
         {
-            isJumping = false;
-            groundPosition = controller.transform.position.y;
+            _isJumping = false;
+            _groundPosition = controller.transform.position.y;
         }
 
         private void Update()
         {
             var inputAxisVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
             var inputAxisHorizontal = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-
             var speedVector = controller.transform.forward * inputAxisVertical;
 
-
             if (controller.isGrounded)
+                VerifyStartJump();
+
+            JumpHandler();
+            MovementHandler(inputAxisVertical, inputAxisHorizontal, speedVector);
+        }
+
+        private void VerifyStartJump()
+        {
+            _groundPosition = controller.transform.position.y;
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                groundPosition = controller.transform.position.y;
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    isJumping = true;
-                }
+                _isJumping = true;
+            }
+        }
+
+        private void JumpHandler()
+        {
+            if (!_isJumping)
+                return;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _jumpCoeficient = jumpForce * Time.deltaTime;
             }
 
-            if (isJumping)
+            if (Input.GetKeyUp(KeyCode.Space) || controller.transform.position.y > jumpLimit + _groundPosition)
             {
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    jumpCoeficient = jumpForce * Time.deltaTime;
-                }
-
-                if (Input.GetKeyUp(KeyCode.Space) || controller.transform.position.y > jumpLimit + groundPosition)
-                {
-                    isJumping = false;
-                    jumpCoeficient = 0;
-                }
+                _isJumping = false;
+                _jumpCoeficient = 0;
             }
+        }
 
-            jumpCoeficient -= gravity * Time.deltaTime;
+        private void MovementHandler(float inputAxisVertical, float inputAxisHorizontal, Vector3 speedVector)
+        {
+            _jumpCoeficient -= gravity * Time.deltaTime;
 
-            speedVector.y = jumpCoeficient;
+            speedVector.y = _jumpCoeficient;
 
             controller.transform.Rotate(0, inputAxisHorizontal, 0);
             controller.Move(speedVector);
