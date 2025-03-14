c682b13ec45411a488a4c0c3ae57404467f4a056
pmartinsesa <pmartinsesa@gmail.com>
2025-03-14 18:03:23 -0300

    .

---
+++ Assets/Scripts/Player/Guns/GunBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player.Guns
+{
+    public class GunBase : MonoBehaviour
+    {
+        protected Transform gunPosition;
+        protected int bulletLimit;
+    
+        protected void OnInit()
+        { }
+    }
+}
---
+++ Assets/Scripts/Player/Guns/SimpleGun.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player.Guns
+{
+    public class SimpleGun : GunBase
+    {
+    }
+}
